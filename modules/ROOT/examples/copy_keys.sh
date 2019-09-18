6f60c54c7bd978c280c2c112ff8c81d2e618e952
James Nocentini <james.nocentini@gmail.com>
2019-09-18 09:07:24 +0000

    remove unused script

--- modules/ROOT/examples/copy_keys.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-cp ././../../../../../server/docs-server/modules/manage/assets/attachments/x509-cert-scripts/ca.pem .
-cp ././../../../../../server/docs-server/modules/manage/assets/attachments/x509-cert-scripts/chain.pem .
-cp ././../../../../../server/docs-server/modules/manage/assets/attachments/x509-cert-scripts/pkey.key .
