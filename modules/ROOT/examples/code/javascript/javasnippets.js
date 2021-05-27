59ff3654f9e793a1d6693a65df848600d700b16e
ibsoln <52778946+ibsoln@users.noreply.github.com>
2021-05-27 13:33:34 +0100

    DOC-8065 -- XATTRS Access Grants https://issues.couchbase.com/browse/DOC-8065
    DOC-8065 -- Xattrs -- Initial Product Notes Text
    
    DOC-8065 -- Xattr access grant in sync function
    
    DOC-8065
    
    DOC-8065 interim
    
    DOC-8065 -- db config title
    
    svDOC-8065 -- pgindx
    
    DOC-8065 -- DB config tweak
    
    DOC-8065 -- 210511-1 concept changes
    
    DOC-8065 -- Inerim 210511-2
    
    DOC-8065 -- Interim 210512 -- grants etc
    
    DOC-8065 -- Interim 210513-1 -- nav changes
    
    DOC-8065 -- Interim 210513-2 -- page index fix
    
    DOC-8065 -- Interim210513-3 -- Rejig the Nav and fix some page index refs
    
    DOC-8065 -- Interim 210513-4 -- Tweak examples
    
    DOC-8065 -- Interim 210517-1 -- Change to xattr pg
    
    DOC-8065 -- Interim 210517-2 -- Change to page-index pg
    
    DOC-8065 -- Interim 210517-3 -- integrate access-control content
    
    DOC-8065 -- Interim 210517-4 -- Integration of access control changes
    
    DOC-8065 -- Interim 210517-6
    
    DOC-8065 -- Interim 210520 -- uprated examples
    
    DOC-8065 -- Interim 210521-1
    
    DOC-8065 -- Interim 26May21
    
    DOC-8065 -- XATTRS Access Grants
    https://issues.couchbase.com/browse/DOC-8065

---
+++ modules/ROOT/examples/code/javascript/javasnippets.js
@@ -0,0 +1,28 @@
+// tag::sync-function-using-xattr[]
+function (doc, oldDoc, meta) { // <.>
+
+  if (meta.xattrs.channelXattr == "undefined") // <.>
+    {
+      console.log("no user_xattr_key defined")
+      channel(null)
+    } else {
+      channel(meta.xattrs.channelXattr) // <.>
+
+
+
+    }
+
+  // Further processing as required ../
+
+// end::sync-function-using-xattr[]
+
+/*
+// tag::sync-function-using-xattr-notation[]
+<.> The meta parameter exposes the user defined user_xattr_key if defined. The item takes the name configured for the database
+<.> Access the meta parameter object to check an xattr exists on this document
+<.> Use the content of the xattr to define the `channels` setting for this document
+// end::sync-function-using-xattr-notation[]
+*/
+
+}
+
