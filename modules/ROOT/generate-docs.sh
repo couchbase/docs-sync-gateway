bd5616ee04ebd22e6f4e43cab2be4c44ed20f96f
Simon Dew <39966290+simon-dew@users.noreply.github.com>
2025-01-23 17:06:44 +0000

    DOC-12841: Metrics REST API doc fixes (#839)
    * Remove client app links
    * Remove swagger UI from Metrics REST API page
    * Fix symbol link in JSON stats
    * Standardize on metrics. (It's an endpoint, not a method.)
    * Fix filetype for YAML partials
    * Add OpenAPI overlays
    * Update generate-docs.sh:
        - Sparse checkout of upstream sync_gateway repo;
        - Use jq to transform Prometheus metrics file to JSON schema;
        - Apply OpenAPI overlay to bundled spec;
        - Build output from overlaid spec;
        - Remove the upstream sync_gateway repo.
    * Updates after review:
        - Use Antora descriptor to get tag name
        - Use git tag to get upstream specs
        - Address if existing tag not on same branch as before
        - Make script robust to running from out of directory
        - Correct source API in bundle step
    * Rebuild output documentation

--- modules/ROOT/generate-docs.sh
+++ modules/ROOT/generate-docs.sh
@@ -2,14 +2,52 @@
 
 set -exuo pipefail
 
-cd $(dirname $0)
+cd $(dirname "${BASH_SOURCE[0]}")
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
+cd "${GIT_ROOT}"
 
-PATH_TO_SYNC_GATEWAY=${PATH_TO_SYNC_GATEWAY:-${GIT_ROOT}/../sync_gateway/}
+# Get docs tag name from current major.minor version
+yq -V || brew install yq
+tag=$(yq -r .version antora.yml)-docs
+
+# Get upstream sync_gateway repo
+git clone --no-checkout https://github.com/couchbase/sync_gateway.git
+cd sync_gateway
+git sparse-checkout init --cone
+git sparse-checkout set docs/api
+# check & delete local tag in case remote tag has been moved
+has_tag=$(git tag -l ${tag})
+if [[ -n ${has_tag} ]]; then
+    git tag -d ${tag}
+fi
+# update remote tags after deleting local ones
+git fetch --tags
+git reset --hard ${tag}
+cd ..
+
+PATH_TO_SYNC_GATEWAY=${PATH_TO_SYNC_GATEWAY:-${GIT_ROOT}/sync_gateway}
 PATH_TO_SPECS="${PATH_TO_SYNC_GATEWAY}/docs/api/"
 
-PATH_TO_BUNDLE=${PATH_TO_BUNDLE:-${GIT_ROOT}/modules/ROOT/assets/attachments/}
+PATH_TO_BUNDLE=${PATH_TO_BUNDLE:-${GIT_ROOT}/modules/ROOT/assets/attachments}
+PATH_TO_OVERLAY=${PATH_TO_OVERLAY:-${GIT_ROOT}/modules/ROOT/assets/overlays}
+PATH_TO_TEMP=${PATH_TO_TEMP:-${GIT_ROOT}/modules/ROOT/assets/bld}
+
+# Create a bld directory to hold temporary files, if it doesn't already exist
+[[ -d "${PATH_TO_TEMP}" ]] || mkdir "${PATH_TO_TEMP}"
+
+# Use jq to transform the Prometheus metrics file into JSON schema.
+# Temporarily omitting the description until we can sort out Redocly CSS issues.
+# Should be {"type": "integer", "format": .value.type, "description": .value.help}
+npx node-jq 'to_entries |
+map(.value += {"type": "integer", "format": .value.type}) |
+map(.value |= del(.added, .labels, .unit, .stability, .help)) |
+from_entries |
+{type: "object", properties: .}' \
+  "${PATH_TO_BUNDLE}/metrics_metadata.json" \
+  > "${PATH_TO_TEMP}/metrics_metadata.json"
+
+cd modules/ROOT
 
 generate() {
     WHAT=$1
@@ -17,14 +55,22 @@ generate() {
     # this bundle may be needed for the database configuration part?
     npx '@redocly/cli' bundle \
         --config "${PATH_TO_SYNC_GATEWAY}/.redocly.yaml" \
-        "${PATH_TO_SYNC_GATEWAY}/docs/api/${WHAT}.yaml" \
+        "${WHAT}" \
         --dereferenced \
         --output "${PATH_TO_BUNDLE}/bundled-${WHAT}.yaml"
 
+    # Get the bundled spec, apply the OpenAPI overlay,
+    # and output to the temporary bld folder.
+    npx openapi-overlays-js \
+        --openapi "${PATH_TO_BUNDLE}/bundled-${WHAT}.yaml" \
+        --overlay "${PATH_TO_OVERLAY}/${WHAT}.yaml" \
+        > "${PATH_TO_TEMP}/${WHAT}.yaml"
+
+    # Build the reference documentation using the overlaid spec.
     npx @redocly/cli \
       build-docs \
       --config redocly.yaml \
-      "${PATH_TO_SYNC_GATEWAY}/docs/api/${WHAT}.yaml" \
+      "${PATH_TO_TEMP}/${WHAT}.yaml" \
       --output partials/sgw-openapi-$WHAT.html \
       --template partials/redocAll.hbs
 }
@@ -32,3 +78,6 @@ generate() {
 generate public
 generate admin
 generate metric
+
+# Clear upstream repo
+rm -rf "${PATH_TO_SYNC_GATEWAY}"
